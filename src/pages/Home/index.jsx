2e1ddd5624001eec0c1a85a69d325c367ac2fa3f
Alois Sailer <alois_sailer@yahoo.com.br>
2022-08-06 20:26:02 -0300

    function onChange

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,10 +4,20 @@ import { Card } from '../../components/Card'
 
 export function Home() {
 
+  function handleNameChange(name) {
+    console.log(name)
+  }
+
   return (
     <div className="container">
       <h1>Lista de Presença</h1>
-      <input type="text" placeholder="Digite o nome..." />
+
+      <input
+        type="text"
+        placeholder="Digite o nome..."
+        onChange={e => handleNameChange(e.target.value)}
+      />
+
       <button type="button">Adicionar</button>
 
       <Card name="Alois" time="10:55:25" />
