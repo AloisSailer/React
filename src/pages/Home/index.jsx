8acfab53d7b2916263c02eb46e4caac0d6e3c7ba
Alois Sailer <alois_sailer@yahoo.com.br>
2022-08-05 21:11:16 -0300

    adicionado className container

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import './style.css'
 export function Home() {
 
   return (
-    <div>
+    <div className="container">
       <h1>Lista de Presença</h1>
       <input type="text" placeholder="Digite o nome..." />
       <button type="button">Adicionar</button>
